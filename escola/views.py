7f6ab9daaaa7d395e0101c09b3a9cb515267a55a
leonardo <leonardosc.me@gmail.com>
2023-12-14 22:48:42 -0300

    Iniciando implementacao das nossas views

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if (request.method == 'GET'):
-        aluno = {
-            'id': 1,
-            'nome': 'Leonardo'
-        }
-        return JsonResponse(aluno)
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
